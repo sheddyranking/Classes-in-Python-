8dcf46180566b505ab45f4d74e2bb1c7648d0324
sheddyranking <shedyisaac@gmail.com>
2022-01-23 00:51:25 +0100

    used a class method to draw shapes base on the parametes passed

--- class_lesson1.py
+++ class_lesson1.py
@@ -1,7 +1,20 @@
+import turtle
+ 
 class Polygon: #the class name always start with a upper case variable 
     def __init__(self, sides, name):
         self.sides = sides
         self.name = name
+
+    #drawing the shape base on the parametres passed into the class.
+    #to do that we define a CLASS METHOD.
+
+    def draw(self):
+        for i in range(self.sides):
+            turtle.forward(100)
+            turtle.right(90)
+        turtle.done()
+
+ 
 
 square = Polygon(4, "sqaure")
 pentagon = Polygon(5, "pentagon")
@@ -12,3 +25,5 @@ print(square.name)
 
 print(pentagon.sides)
 print(pentagon.name)
+
+square.draw()
